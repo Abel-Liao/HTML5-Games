5e1017fe6af843239d47551de0e951a8f931234d
Abel <1043477154@qq.com>
2018-05-01 11:41:40 +0800

    boxGame

--- boxGames/js/boxGames.js
+++ boxGames/js/boxGames.js
@@ -8,17 +8,7 @@ window.onload = function(){
     var previousLevel = document.getElementById("previousLevel");//上一关
     var nextLevel = document.getElementById("nextLevel");//下一关
     var reset = document.getElementById("reset");//重玩当前关
-
-    var bg = document.getElementById("bg");
-    var tree = document.getElementById("tree");
-    var succrss = document.getElementById("succrss");
-    var box = document.getElementById("box");
-    var down = document.getElementById("down");
-    var left = document.getElementById("left");
-    var up = document.getElementById("up");
-    var right = document.getElementById("right");
-
-
+    var bg,tree,succrss,box,down,left,up,right;
     var checkpoint = 1; //记录关数
     var sunwukong,boxPostions,treePostions,succrssPostions;
     //   猴哥位置  所有箱子位置 所有树木位置  所有成功圆位置
@@ -56,27 +46,56 @@ window.onload = function(){
         up:"images/up.png",
         right:"images/right.png"
     };
-    // 绘制图片Fun
-    var imgFun = function(id,xPostion,yPostion){
-        if(id==down || id==up || id==left || id==right){ // 判断是否为绘制猴哥
-            this.xPostion = xPostion-7.5;
-            this.yPostion = yPostion-27;
-        }else if(id==box || id==tree){ // 判断是否为绘制箱子 或者 大树
-            this.xPostion = xPostion;
-            this.yPostion = yPostion-11;
-        }else if(id==succrss){// 判断是否为绘制圆
-            this.xPostion = xPostion+3;
-            this.yPostion = yPostion+2;
-        }else{
-            this.xPostion = xPostion;
-            this.yPostion = yPostion;
+    // 预加载img标签
+    function imgFun(id,name,x,y){
+        id=new Image();
+        switch (name){
+            case "bg":
+                id.src = pic.bg;
+                break;
+            case "tree":
+                y -= 11;
+                id.src = pic.tree;
+                break;
+            case "box":
+                y -= 11;
+                id.src = pic.box;
+                break;
+            case "succrss":
+                x += 3;
+                y += 2;
+                id.src = pic.succrss;
+                break;
+            case "down":
+                x += -7.5;
+                y += -27;
+                id.src = pic.down;
+                break;
+            case "left":
+                x += -7.5;
+                y += -27;
+                id.src = pic.left;
+                break;
+            case "up":
+                x += -7.5;
+                y += -27;
+                id.src = pic.up;
+                break;
+            case "right":
+                x += -7.5;
+                y += -27;
+                id.src = pic.right;
+                break;
         }
-        boxGame.drawImage(id,this.xPostion,this.yPostion);
+        // console.log(id);
+        id.onload = function(){
+            boxGame.drawImage(id,x,y);
+        }
     }
     // 绘制底图背景
     var floorFun = function(){
         for(var i=0,x=0,y=0; i=true; i++){
-            imgFun(bg,x,y);
+            imgFun(bg,"bg",x,y);
             if(x>560){
                 x=0;
                 y+=35;
@@ -93,22 +112,22 @@ window.onload = function(){
         // 绘制底图背景
         floorFun();
         // 绘制树木
-        drawAny(tree,treePostions[checkpoint-1]);
+        drawAny(tree,"tree",treePostions[checkpoint-1]);
         // 绘制目的圆
-        drawAny(succrss,succrssPostions[checkpoint-1]);
+        drawAny(succrss,"succrss",succrssPostions[checkpoint-1]);
         // 绘制箱子
-        drawAny(box,boxPostions[checkpoint-1]);
+        drawAny(box,"box",boxPostions[checkpoint-1]);
         // 绘制猴哥
-        imgFun(direction,sunwukong[checkpoint-1][0],sunwukong[checkpoint-1][1]);
+        imgFun("down",direction,sunwukong[checkpoint-1][0],sunwukong[checkpoint-1][1]);
     }
     // 绘制多个相同目标图片
-    var drawAny = function (name,postion){
+    var drawAny = function (id,name,postion){
         for(var i=0;i<postion.length;i++){
-            imgFun(name,postion[i][0],postion[i][1]);
+            imgFun(id,name,postion[i][0],postion[i][1]);
         }
     }
     // 绘制整个页面
-    allPic(down);
+    allPic("down");
      // 游戏成功判断
     var succrssFun = function(){
         var abc = [];
@@ -135,19 +154,19 @@ window.onload = function(){
         var walk = true; // 箱子移动判断依据
         if(keyNum == 39 || keyNum == 40 || keyNum == 38 || keyNum == 37){
             if(keyNum == 40){ // 下
-                lrtmMobile(1,down);
+                lrtmMobile(1,"down");
             }else if(keyNum == 38){ // 上
-                lrtmMobile(-1,up);
+                lrtmMobile(-1,"up");
             }else if(keyNum == 37){ // 左
-                lrtmMobile(-1,left);
+                lrtmMobile(-1,"left");
             }else if(keyNum == 39){ // 右
-                lrtmMobile(1,right);
+                lrtmMobile(1,"right");
             }
         }
     }
     // 猴哥移动
     var lrtmMobile = function(value,direction){
-        if(direction==left || direction==right){
+        if(direction=="left" || direction=="right"){
             sunwukong[checkpoint-1][0]+=value*35;
         }else{
             sunwukong[checkpoint-1][1]+=value*35;
@@ -170,7 +189,7 @@ window.onload = function(){
     }
     //让猴哥保持原地不动
     var judgmentDirection = function(value,direction){
-        if(direction==left || direction==right){
+        if(direction=="left" || direction=="right"){
             sunwukong[checkpoint-1][0]-=value*35;
         }else{
             sunwukong[checkpoint-1][1]-=value*35;
@@ -178,7 +197,7 @@ window.onload = function(){
     }
     // 判断是否有障碍物
     var isObstacle = function(value,direction,name,i){// name 为判断障碍物
-        var directionChoos = direction==left || direction==right;
+        var directionChoos = direction=="left" || direction=="right";
         for(var r=0;r<name.length;r++){
             var varName = directionChoos?(boxPostions[checkpoint-1][i][1]==name[r][1] && ((boxPostions[checkpoint-1][i][0]+35*value)==name[r][0])):((boxPostions[checkpoint-1][i][1]+35*value)==name[r][1] && boxPostions[checkpoint-1][i][0]==name[r][0]);
             if(varName){
@@ -190,9 +209,9 @@ window.onload = function(){
     }
     // 猴哥推动箱子移动
     var boxMobile = function(value,direction,num){
-        if(direction==right || direction==left){//判断横向或者纵向
+        if(direction=="right" || direction=="left"){//判断横向或者纵向
             boxPostions[checkpoint-1][num] = [boxPostions[checkpoint-1][num][0]+35*value,boxPostions[checkpoint-1][num][1]];
-        }else if(direction==up || direction==down){
+        }else if(direction=="up" || direction=="down"){
             boxPostions[checkpoint-1][num] = [boxPostions[checkpoint-1][num][0],boxPostions[checkpoint-1][num][1]+35*value];
         }
         succrssFun();
--- boxGames/js/boxGames_02.js
+++ boxGames/js/boxGames_02.js
@@ -8,18 +8,9 @@
     var treePostion = [[35*5,35*7],[35*7,35*4]];// 树木位置
     var succrssPostion = [[35*7,35*7],[35*5,35*3],[35*8,35*4]];// 成功圆位置
     // 绘制地图
-    // var ab = new Image();
+    var bg,tree;
+    // var bg = new Image();
     // var tree = new Image();
-    var bgc = {
-        ab: new Image(),
-        tree: new Image(),
-        succrss: new Image(),
-        box: new Image(),
-        down: new Image(),
-        left: new Image(),
-        up: new Image(),
-        right: new Image(),
-    }
     var ac = {
         bg:"images/block.gif",
         tree:"images/wall.png",
@@ -29,59 +20,69 @@
         left:"images/left.png",
         up:"images/up.png",
         right:"images/right.png"
-    }
+    };
     // var addFun = function(id){
         // id.onload = function(){
         //     abcd();
         // }
     // }
+    function imgAdd(id,name,x,y){
+        id=new Image();
+        switch (name){
+            case "bg":
+                id.src = ac.bg;
+                break;
+            case "tree":
+                id.src = ac.tree;
+                break;
+        }
+        // console.log(id);
+        id.onload = function(){
+            boxGame.drawImage(id,x,y);
+        }
+    }
+    // imgAdd(bg,"bg");
+    // imgAdd(tree,"tree");
     // 绘制图片Fun
     var imgFun = function(id,xPostion,yPostion){
-        
-        // id.onload = function(){
-            if(id==bgc.ab){
-                bgc.ab.src = ac.bg;
-            }else if(id==bgc.tree){
-                bgc.tree.src = ac.tree;
-            }
-            this.xPostion = xPostion;
-            this.yPostion = yPostion;
+        console.log(id);
+        if(id== bg){
+            bg.src = ac.bg;
+        }else if(id==tree){
+            tree.src = ac.tree;
+        }
+        this.xPostion = xPostion;
+        this.yPostion = yPostion;
+        // console.log(id);
+        id.onload = function(){
             boxGame.drawImage(id,this.xPostion,this.yPostion);
-            abcd();
-        // }
-        // addFun(id);
+        }
     }
     // 绘制底图背景
     var floorFun = function(){
+        console.log(11);
         for(var i=0,x=0,y=0; i=true; i++){
-            imgFun(bgc.ab,x,y);
-            bgc.bg.onload = function(){
-                if(x>560){
-                    x=0;
-                    y+=35;
-                    if(y>560){
-                        break;
-                    }
-                }else{
-                    x+=35;
+            // imgFun(bg,x,y);
+            imgAdd(bg,"bg",x,y);
+            if(x>560){
+                x=0;
+                y+=35;
+                if(y>560){
+                    break;
                 }
+            }else{
+                x+=35;
             }
-            
         }
     }
     var drawAny = function (name,postion){
         for(var i=0;i<postion.length;i++){
-            imgFun(name,postion[i][0],postion[i][1]);
+            // imgFun(name,postion[i][0],postion[i][1]);
+            imgAdd(tree,"tree",postion[i][0],postion[i][1]);
         } 
     }
     var abcd = function(){
         floorFun();
-        drawAny(bgc.tree,treePostion);
+        drawAny(tree,treePostion);
     }
     abcd();
-    // 绘制整个页面
-    // var allPic = function(){// direction 为判断猴哥面向图片
-    //     // 绘制底图背景
-    //     abcd();
-    // }
-    // 绘制整个页面
